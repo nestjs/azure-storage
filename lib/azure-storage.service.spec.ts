b1e29b2c99215801011c176007f72f7bcebb9d6c
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2021-07-28 08:53:46 +0200

    test(): fix unit test

--- lib/azure-storage.service.spec.ts
+++ lib/azure-storage.service.spec.ts
@@ -122,7 +122,7 @@ describe('AzureStorageService', () => {
       await storage.upload(null);
     } catch (e) {
       expect(e.toString()).toBe(
-        "TypeError: Cannot destructure property `buffer` of 'undefined' or 'null'.",
+        "TypeError: Cannot destructure property 'buffer' of 'file' as it is null.",
       );
     }
   });
